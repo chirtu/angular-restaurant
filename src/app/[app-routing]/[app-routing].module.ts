f3e7fc30aa9986f5651720583ce67465fcc576d0
fatuma.chitu <chitufatuma45@gmail.com>
2022-03-17 21:35:32 +0300

    second commit

--- src/app/[app-routing]/[app-routing].module.ts
+++ src/app/[app-routing]/[app-routing].module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { [appRouting]RoutingModule } from './[app-routing]-routing.module';
+import { [appRouting]import { from } from 'rxjs';
+RoutingModule } from './[app-routing]-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    [appRouting]RoutingModule
+    [appRouting]RoutingModule,
   ]
 })
 export class [appRouting]Module { }
